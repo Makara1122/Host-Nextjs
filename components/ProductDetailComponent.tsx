ed3c3a7bc5ebe3b96c94a6712bd6b7033be2ebec
Your Name <you@example.com>
2024-03-30 15:11:53 +0700

    create robots.txt

--- components/ProductDetailComponent.tsx
+++ components/ProductDetailComponent.tsx
@@ -3,8 +3,8 @@ import { Card } from 'flowbite-react';
 
 export default function ProductDetailComponent({image, title, price}: {image: string, title: string, price: number}) {
   return (
-    <Card className="max-w-sm " imgSrc={image} horizontal>
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <Card className="max-w-sm grid place-content-center" imgSrc={image} horizontal>
+      <h5 className=" font-bold tracking-tight text-gray-900 dark:text-white">
       {title}
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
